3c19efa9f650c6f3a6ea1a6b9abef830712e29d6
Konstantin Pogorelov <cpogorelov@gmail.com>
2012-12-25 19:15:45 +0100

    collision categories, crate gathering

---
+++ src/zp/engine/Protagonist.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var
+    geo      = require('pointExtension'),
+    ccp      = geo.ccp,
+    flame     = require('flame')
+
+function Protagonist(viewport) {
+	Protagonist.superclass.constructor.call(this, viewport);
+}
+
+Protagonist.inherit(flame.engine.Protagonist, {
+	processObjectEventQueue: function() {
+		for (var i in this.fe.objectEventQueue) {
+			var item = this.fe.objectEventQueue[i];
+			if (item.type == 'gather') {
+				var node = item.thing.nodes.obstacle,
+					aniDef1 = {action: 'MoveBy', duration: 0.15, location: geo.ccpSub(this.ego.location, item.thing.location)},
+					aniDef2 = {action: 'Remove', delay: 0.16};
+				this.fe.nodeBuilder.animateNode(node, aniDef1, this.viewport.obstacle);
+				this.fe.nodeBuilder.animateNode(node, aniDef2, this.viewport.obstacle);
+				this.fe.removeThing(item.thing, true);
+			}
+		}
+		this.fe.objectEventQueue = [];
+	},
+	setFieldEngine: function(fe) {
+		Protagonist.superclass.setFieldEngine.call(this, fe);
+		fe.preStepPlugins.push(this.processObjectEventQueue.bind(this));
+	}
+});
+
+module.exports = Protagonist;
